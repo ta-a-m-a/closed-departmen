a76787a1b1afaa4ed23bd709ff780a61f61cb038
ta-a-m-a <taham.ahmet@gmail.com>
2026-04-21 15:11:28 +0300

    jj

---
+++ script.js
@@ -0,0 +1,47 @@
+
+let deneme = 20;
+
+let x 
+
+x= 30;
+
+let my_dog;
+
+console.log(deneme)
+
+console.log(x)
+
+console.log(my_dog)
+
+my_dog = 50 
+
+let greetings = "Hello "
+
+let name = "Alice"
+
+let space = "  "
+
+let mario = "87"
+
+let superb = " 54"
+
+
+supermario = superb + mario
+
+console.log(supermario)
+
+hasJob = false
+
+
+console .log(deneme + my_dog)
+
+let total = deneme+ my_dog
+
+let h = greetings + space + isim 
+
+console.log(h)
+
+mario = 30;
+
+console.lof(mario)
+
